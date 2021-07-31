e6ba3ff33f6a8f131a90519db3082eb3957e9a54
acesar-l <acesar-l@student.42sp.org.br>
2021-07-31 10:26:29 +0000

    ultimos envios

--- c08/ex00/ft.h
+++ c08/ex00/ft.h
@@ -1,61 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/31 10:25:58 by acesar-l          #+#    #+#             */
+/*   Updated: 2021/07/31 10:25:59 by acesar-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FT_H
 # define FT_H
-# include <unistd.h>
+
 void	ft_putchar(char c);
 void	ft_swap(int *a, int *b);
 void	ft_putstr(char *str);
 int		ft_strlen(char *str);
 int		ft_strcmp(char *s1, char *s2);
-
-void	ft_putchar(char c)
-{
-	write (1, &c, 1);
-}
-
-void	ft_swap(int *a, int *b)
-{
-	int	aux;
-
-	aux = *a;
-	*a = *b;
-	*b = aux;
-}
-
-void	ft_putstr(char *str)
-{
-	while (*str)
-	{
-		write(1, str, 1);
-		str++;
-	}
-}
-
-int	ft_strlen(char *str)
-{
-	int	count;
-
-	count = 0;
-	while (*str)
-	{
-		++count;
-		++str;
-	}
-	return (count);
-}
-
-int	ft_strcmp(char *s1, char *s2)
-{
-	unsigned char	*p1;
-	unsigned char	*p2;
-
-	p1 = (unsigned char *) s1;
-	p2 = (unsigned char *) s2;
-	while (*p1 && *p1 == *p2)
-	{
-		p1++;
-		p2++;
-	}
-	return (*p1 - *p2);
-}
 
 #endif
--- c08/ex01/ft_boolean.h
+++ c08/ex01/ft_boolean.h
@@ -1,16 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/31 10:25:26 by acesar-l          #+#    #+#             */
+/*   Updated: 2021/07/31 10:25:27 by acesar-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FT_BOOLEAN_H
 # define FT_BOOLEAN_H
-# define EVEN(nbr) nbr % 2
+# define EVEN(x) (((x) % 2 == 0)
 # define EVEN_MSG "I have an even number of arguments."
 # define ODD_MSG "I have an odd number of arguments."
 # define SUCCESS 0
-
-	const int TRUE;
-	const int FALSE;
-	const int SUCCESS;
+# define TRUE 1
+# define FALSE 0 
 	
-	TRUE = 1;
-	FALSE = 0;
-	SUCCESS = 0;
+	typedef int t_bool;
 
 #endif
